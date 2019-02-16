d16d9d0b62d158cc9f7f973f19c5aad081db5d81
Stanley Bartlett <stanbartlett@gmail.com>
2019-02-16 16:33:27 -0500

    die hard

---
+++ lectures/week6/tests.scala
@@ -0,0 +1,9 @@
+package week7
+
+object test {
+  val problem = new Pouring(Vector(4, 7, 9))
+  println(problem.moves)
+
+  println(problem.pathSets.take(3).toList)
+  println(problem.solutions(6)
+}
